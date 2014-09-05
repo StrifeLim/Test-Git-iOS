5a4f9e3d4b1ea828b1e920096a4bf423a6f50b54
Strife Lim <strifelim@gmail.com>
2014-09-06 01:09:11 +0800

    added a test class

---
+++ SSTest.h
@@ -0,0 +1,13 @@
+//
+//  SSTest.h
+//  gittest
+//
+//  Created by Strife Lim Wei Hou on 6/9/14.
+//  Copyright (c) 2014 ___HOUYOYOYO___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSTest : NSObject
+
+@end
---
+++ SSTest.m
@@ -0,0 +1,13 @@
+//
+//  SSTest.m
+//  gittest
+//
+//  Created by Strife Lim Wei Hou on 6/9/14.
+//  Copyright (c) 2014 ___HOUYOYOYO___. All rights reserved.
+//
+
+#import "SSTest.h"
+
+@implementation SSTest
+
+@end
